e41f8cfb13bc95b32f0278cd042fd64454575c5b
Abeeku Djokoto <djokotoabeeku619@gmail.com>
2024-08-03 15:21:55 +0000

    rm: removed console logs

--- src/components/detailedListingCard/DetailedListing.jsx
+++ src/components/detailedListingCard/DetailedListing.jsx
@@ -2,7 +2,6 @@ import PropTypes from "prop-types";
 import { HeartIcon } from "../../assets";
 
 const DetailedListingCard = (props) => {
-  console.log("props", props);
   const { image, title, location, price, amenities } = props.data;
 
   return (
